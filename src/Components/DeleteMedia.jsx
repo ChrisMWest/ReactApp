ebdf7d766eae179df2116065228e3965ef6ae91d
ChrisMWest <cmwest135@gmail.com>
2023-02-21 20:00:37 -0800

    Deleted unused files

--- src/Components/DeleteMedia.jsx
+++
@@ -1,5 +0,0 @@
-import React,{useState, useEffect, useMemo} from 'react';
-import './App.css';
-import axios from "axios";
-import { FormControlLabel, IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
